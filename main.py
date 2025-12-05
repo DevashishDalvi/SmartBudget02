9e007880f2faff54ec7c8774b88d13ebfde69c17
Devashish Dalvi <92207754+DevashishDalvi@users.noreply.github.com>
2025-12-05 18:58:22 +0530

    Commit: Project save
    - Boo Boo Baa Baa

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import logging
+
+from loguru import logger
+
+from src.SmartBudget.utils.logger import setup_logging
+
+setup_logging()
+
+
 def main() -> None:
-    print("Hello from smartbudget02!")  # noqa: T201
+    logger.info("Hello from smartbudget02!")
 
 
 if __name__ == "__main__":
     main()
+    std_logger = logging.getLogger("some_lib")
+    std_logger.warning("some_lib warning!!")
